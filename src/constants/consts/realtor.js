aea8c2e30c1270d959f7f400b1c3eb46f014b68d
Fabian Salas <fabian.salas.astete@gmail.com>
2024-03-30 19:12:35 -0300

    first commit

---
+++ src/constants/consts/realtor.js
@@ -0,0 +1,11 @@
+export const realtorData = {
+  email: 'immobiliare@unne.cl',
+  phone:'+56997871527'
+  // email: 'contacto@accionpanal.com',
+};
+
+export const footerData = {
+  email: 'immobiliare@unne.cl',
+  phone:'997871527',
+  wsp:'997871527',
+}
